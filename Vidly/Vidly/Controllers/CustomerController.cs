ce4d0fa9b6cd59be333be30e7f69dd60b4e6bbb5
Nazrul.635 <Nazrul.635@datasoft-bd.com>
2018-07-09 17:10:30 +0600

    Viewing the Discount Rate on the Customer table. CustomerController and view customer index

--- Vidly/Vidly/Controllers/CustomerController.cs
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,26 +12,30 @@ namespace Vidly.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
+
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-            return View(customers);
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customer);
         }
         public ActionResult Details (int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
-            return View();
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name = "Moinul Islam"}
-            };
-        }
+            return View(customer);
+        }        
     }
 }
